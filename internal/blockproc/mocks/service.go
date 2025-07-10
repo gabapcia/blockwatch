0b8b6f703dcd56c2362476cbe0b137d6999a204e
Gabriel <gabapcia@gmail.com>
2025-07-09 21:54:33 -0300

    test(blockproc): create mocks

--- internal/blockproc/mocks/service.go
+++ internal/blockproc/mocks/service.go
@@ -7,7 +7,6 @@ package mocks
 import (
 	"context"
 
-	"github.com/gabapcia/blockwatch/internal/chainstream"
 	mock "github.com/stretchr/testify/mock"
 )
 
@@ -72,31 +71,20 @@ func (_c *Service_Close_Call) RunAndReturn(run func()) *Service_Close_Call {
 }
 
 // Start provides a mock function for the type Service
-func (_mock *Service) Start(ctx context.Context) (<-chan chainstream.ObservedBlock, error) {
+func (_mock *Service) Start(ctx context.Context) error {
 	ret := _mock.Called(ctx)
 
 	if len(ret) == 0 {
 		panic("no return value specified for Start")
 	}
 
-	var r0 <-chan chainstream.ObservedBlock
-	var r1 error
-	if returnFunc, ok := ret.Get(0).(func(context.Context) (<-chan chainstream.ObservedBlock, error)); ok {
-		return returnFunc(ctx)
-	}
-	if returnFunc, ok := ret.Get(0).(func(context.Context) <-chan chainstream.ObservedBlock); ok {
+	var r0 error
+	if returnFunc, ok := ret.Get(0).(func(context.Context) error); ok {
 		r0 = returnFunc(ctx)
 	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(<-chan chainstream.ObservedBlock)
-		}
+		r0 = ret.Error(0)
 	}
-	if returnFunc, ok := ret.Get(1).(func(context.Context) error); ok {
-		r1 = returnFunc(ctx)
-	} else {
-		r1 = ret.Error(1)
-	}
-	return r0, r1
+	return r0
 }
 
 // Service_Start_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'Start'
@@ -123,12 +111,12 @@ func (_c *Service_Start_Call) Run(run func(ctx context.Context)) *Service_Start_
 	return _c
 }
 
-func (_c *Service_Start_Call) Return(observedBlockCh <-chan chainstream.ObservedBlock, err error) *Service_Start_Call {
-	_c.Call.Return(observedBlockCh, err)
+func (_c *Service_Start_Call) Return(err error) *Service_Start_Call {
+	_c.Call.Return(err)
 	return _c
 }
 
-func (_c *Service_Start_Call) RunAndReturn(run func(ctx context.Context) (<-chan chainstream.ObservedBlock, error)) *Service_Start_Call {
+func (_c *Service_Start_Call) RunAndReturn(run func(ctx context.Context) error) *Service_Start_Call {
 	_c.Call.Return(run)
 	return _c
 }
