7bc89b8cd8f03c702c2155b43d5dfac29726a3bf
Gabriel <gabapcia@gmail.com>
2025-07-24 23:11:46 -0300

    feat(solana): add solana to config

--- internal/pkg/config/blockchain/blockchain.go
+++ internal/pkg/config/blockchain/blockchain.go
@@ -6,9 +6,13 @@ import "github.com/gabapcia/blockwatch/internal/pkg/config/pkg"
 const (
 	// ProviderEthereum identifies the Ethereum network in configuration or selection logic.
 	ProviderEthereum = "ETHEREUM"
+
+	// ProviderSolana identifies the Solana network in configuration or selection logic.
+	ProviderSolana = "SOLANA"
 )
 
 // Networks defines the supported blockchain network configurations.
 type Networks struct {
 	Ethereum *pkg.JsonRPC `env:", prefix=ETHEREUM_" validate:"omitempty"` // Ethereum holds the JSON-RPC configuration for the Ethereum network.
+	Solana   *pkg.JsonRPC `env:", prefix=SOLANA_" validate:"omitempty"`   // Solana holds the JSON-RPC configuration for the Solana network.
 }
